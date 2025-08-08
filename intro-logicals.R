b4928818e8925308742f0a1f71a6b598a0f7837d
NatBonnet <nathaliehbonnet@gmail.com>
2025-08-08 13:53:44 -0700

    logical practice

---
+++ intro-logicals.R
@@ -0,0 +1,23 @@
+rm(list = ls())
+
+dogs <- c(1, 10, 4, 5, 18)
+cats <- c(0, 12, 10, 5, 2)
+
+dogs < cats #assessing this element by element
+
+5 == TRUE
+1== TRUE
+0 == FALSE
+
+5 != 6
+
+5 < 10& "cat" < "dog"
+
+bananas <- c(10, 21, 12, 15, 22)
+apples <- c(9, 18, 16, 20, 30)
+
+apples < bananas & bananas > 20
+
+4 <10 | 100 <2
+
+5 > 6 | "dog" < "cat" | 10 < 50
